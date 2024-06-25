df840a677e2fffe4e03a657e1676f5eebc583ca5
MBURKU <rku@aarhus.dk>
2024-06-25 13:55:36 +0200

    Updated process.py

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -14,93 +14,89 @@ from robot_framework.case_manager.case_handler import CaseHandler
 def process(orchestrator_connection: OrchestratorConnection) -> None:
     """Do the primary process of the robot."""
     orchestrator_connection.log_trace("Running process.")
+    oc_args_json = json.loads(orchestrator_connection.process_arguments)
+    orchestrator_connection.log_info(orchestrator_connection.get_constant('test_uuid').value)
 
+    uuid = orchestrator_connection.get_constant('test_uuid').value
+    ssn = orchestrator_connection.get_credential('test_person').password
 
-if __name__ == "__main__":
-    oc = OrchestratorConnection.create_connection_from_args()
-    oc_args_json = json.loads(oc.process_arguments)
-    oc.log_info(oc.get_constant('test_UUID').value)
+    if not any(ssn) or not any(uuid):
+        raise ValueError("No ssn given. ")
 
-    UUID = oc.get_constant('test_UUID').value
-    SSN = oc.get_credential('test_person').password
-
-    if not any(SSN) or not any(UUID):
-        raise ValueError("No SSN given. ")
-
-    ENDPOINT = os.getenv('GoApiBaseUrl')
-    USERNAME = oc.get_credential('go_api').username
-    PASSWORD = oc.get_credential('go_api').password
-    CONN_STRING = oc.get_constant('DbConnectionString').value
-    TABLE_NAME = oc_args_json['table_name']
-    CASE_TYPE = oc_args_json['case_type']
-    STATUS_SP = oc_args_json['status_sp']
-    DB_UPDATE_SP = oc_args_json['db_update_sp']
-    STATUS_PARAMS_FAILED = {
+    endpoint = os.getenv('GoApiBaseUrl')
+    username = orchestrator_connection.get_credential('go_api').username
+    password = orchestrator_connection.get_credential('go_api').password
+    conn_string = orchestrator_connection.get_constant('DbConnectionString').value
+    table_name = oc_args_json['table_name']
+    case_type = oc_args_json['case_type']
+    status_sp = oc_args_json['status_sp']
+    db_update_sp = oc_args_json['db_update_sp']
+    status_params_failed = {
         "Status": "FAILED",
-        "UUID": f'{UUID}',
-        "TableName": f'{TABLE_NAME}'
+        "uuid": f'{uuid}',
+        "TableName": f'{table_name}'
     }
-    PERSON_FULL_NAME = None
-    PERSON_GO_ID = None
+    person_full_name = None
+    person_go_id = None
 
-    case_handler = CaseHandler(ENDPOINT, USERNAME, PASSWORD)
+    case_handler = CaseHandler(endpoint, username, password)
 
     # 1. contact lookup
-    oc.log_info("contact lookup")
-    contact_lookup_response = case_handler.contact_lookup(SSN, '/borgersager/_goapi/contacts/readitem')
+    orchestrator_connection.log_info("contact lookup")
+    contact_lookup_response = case_handler.contact_lookup(ssn, '/borgersager/_goapi/contacts/readitem')
 
     if contact_lookup_response.ok:
-        PERSON_FULL_NAME = contact_lookup_response.json()["FullName"]
-        PERSON_GO_ID = contact_lookup_response.json()["ID"]
+        person_full_name = contact_lookup_response.json()["FullName"]
+        person_go_id = contact_lookup_response.json()["ID"]
         response_data_params = {
             "StepName": ("str", "contact_lookup"),
-            "JsonFragment": ("str", f'{{"ContactId": "{PERSON_GO_ID}"}}'),
-            "UUID": ("str", f'{UUID}'),
-            "TableName": ("str", f'{TABLE_NAME}')
+            "JsonFragment": ("str", f'{{"ContactId": "{person_go_id}"}}'),
+            "uuid": ("str", f'{uuid}'),
+            "TableName": ("str", f'{table_name}')
         }
-        db_update_result = execute_stored_procedure(CONN_STRING, DB_UPDATE_SP, response_data_params)
+        db_update_result = execute_stored_procedure(conn_string, db_update_sp, response_data_params)
         if db_update_result['success'] is False:
-            execute_stored_procedure(CONN_STRING, STATUS_SP, STATUS_PARAMS_FAILED)
+            execute_stored_procedure(conn_string, status_sp, status_params_failed)
     else:
-        execute_stored_procedure(CONN_STRING, STATUS_SP, STATUS_PARAMS_FAILED)
+        execute_stored_procedure(conn_string, status_sp, status_params_failed)
 
     case_data_handler = CaseDataJson()
 
     # 2. check if case folder exists
-    oc.log_info("check if case folder exists")
-    search_data = case_data_handler.search_case_folder_data_json(CASE_TYPE, PERSON_FULL_NAME, PERSON_GO_ID, SSN)
+    orchestrator_connection.log_info("check if case folder exists")
+    search_data = case_data_handler.search_case_folder_data_json(case_type, person_full_name, person_go_id, ssn)
     search_case_folder_response = case_handler.search_for_case_folder(search_data, '/_goapi/cases/findbycaseproperties')
     if search_case_folder_response.ok:
         case_folder_id = search_case_folder_response.json()['CasesInfo'][0]['CaseID']
         response_data_params = {
             "StepName": ("str", "find_by_case_properties"),
             "JsonFragment": ("str", f'{{"CaseFolderId": "{case_folder_id}"}}'),
-            "UUID": ("str", f'{UUID}'),
-            "TableName": ("str", f'{TABLE_NAME}')
+            "uuid": ("str", f'{uuid}'),
+            "TableName": ("str", f'{table_name}')
         }
-        db_update_result = execute_stored_procedure(CONN_STRING, DB_UPDATE_SP, response_data_params)
+        db_update_result = execute_stored_procedure(conn_string, db_update_sp, response_data_params)
         if db_update_result['success'] is False:
-            execute_stored_procedure(CONN_STRING, STATUS_SP, STATUS_PARAMS_FAILED)
+            execute_stored_procedure(conn_string, status_sp, status_params_failed)
     else:
-        execute_stored_procedure(CONN_STRING, STATUS_SP, STATUS_PARAMS_FAILED)
+        execute_stored_procedure(conn_string, status_sp, status_params_failed)
 
     # 3. create case folder
     if not case_folder_id:
-        oc.log_info("create case folder")
-        case_folder_data = case_handler.create_case_folder_data(CASE_TYPE, PERSON_FULL_NAME, PERSON_GO_ID, SSN)
+        orchestrator_connection.log_info("create case folder")
+        case_folder_data = case_handler.create_case_folder_data(case_type, person_full_name, person_go_id, ssn)
         create_case_folder_response = case_handler.create_case_folder(case_folder_data, '/_goapi/Cases')
         case_folder_id = create_case_folder_response['CaseID']
 
     # 4. create case
-    oc.log_info("create case")
-    match oc.process_name:
+    orchestrator_connection.log_info("create case")
+    match orchestrator_connection.process_name:
         case "Journalisering_Modersmaal":
-            case_title = f"Modersmålsundervisning {PERSON_FULL_NAME}"
+            case_title = f"Modersmålsundervisning {person_full_name}"
         case "Journalisering_indmeldelse_i_modtagelsesklasse":
-            case_title = f"Visitering af {PERSON_FULL_NAME} {SSN}"
+            case_title = f"Visitering af {person_full_name} {ssn}"
 
     case_data = case_handler.create_case_data(
-        CASE_TYPE,
+        case_type,
         oc_args_json['case_owner_id'],
         oc_args_json['case_owner_name'],
         oc_args_json['case_profile_id'],
@@ -117,3 +113,8 @@ if __name__ == "__main__":
     )
     case_response = case_handler.create_case(case_data, '/_goapi/Cases')
     print(case_response.content)
+
+
+if __name__ == "__main__":
+    oc = OrchestratorConnection.create_connection_from_args()
+    process(oc)
