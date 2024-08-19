053ed3380b1854a5594136ae5228dbed16880bff
MBURKU <rku@aarhus.dk>
2024-08-19 16:54:13 +0200

    Changed code to have less statements.

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,5 +1,4 @@
 """This module contains the main process of the robot."""
-import os
 import json
 
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
@@ -27,33 +26,11 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
         orchestrator_connection.log_trace(f"UUID: {uuid}")
 
         parsed_form = json.loads(form['data'])
-        match oc_args_json['os2form_webform_id']:
-            case "tilmelding_til_modersmaalsunderv" | "indmeldelse_i_modtagelsesklasse" | "ansoegning_om_koersel_af_skoleel" | "ansoegning_om_midlertidig_koerse":
-                if 'cpr_barnets_nummer' in parsed_form['data']:
-                    ssn = parsed_form['data']['cpr_barnets_nummer'].replace('-', '')
-                elif 'barnets_cpr_nummer' in parsed_form['data']:
-                    ssn = parsed_form['data']['barnets_cpr_nummer'].replace('-', '')
-            case _:
-                ssn = None
-
+        ssn = extract_ssn(oc_args_json, parsed_form)
         person_full_name = None
         case_folder_id = None
 
-        status_params_inprogress = {
-            "Status": ("str", "InProgress"),
-            "uuid": ("str", f'{uuid}'),
-            "TableName": ("str", f'{oc_args_json["table_name"]}')
-        }
-        status_params_success = {
-            "Status": ("str", "Successful"),
-            "uuid": ("str", f'{uuid}'),
-            "TableName": ("str", f'{oc_args_json["table_name"]}')
-        }
-        status_params_failed = {
-            "Status": ("str", "Failed"),
-            "uuid": ("str", f'{uuid}'),
-            "TableName": ("str", f'{oc_args_json["table_name"]}')
-        }
+        status_params_inprogress, status_params_success, status_params_failed = get_status_params(uuid, oc_args_json)
 
         execute_stored_procedure(credentials['sql_conn_string'], oc_args_json['hub_update_process_status'], status_params_inprogress)
 
@@ -152,6 +129,64 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
             continue
 
         execute_stored_procedure(credentials['sql_conn_string'], oc_args_json['hub_update_process_status'], status_params_success)
+
+
+def get_status_params(uuid, oc_args_json):
+    """
+    Generates a set of status parameters for the process, based on the given UUID and JSON arguments.
+
+    Args:
+        uuid (str): The unique identifier for the current process.
+        oc_args_json (dict): A dictionary containing various process-related arguments, including table names.
+
+    Returns:
+        tuple: A tuple containing three dictionaries:
+            - status_params_inprogress: Parameters indicating that the process is in progress.
+            - status_params_success: Parameters indicating that the process completed successfully.
+            - status_params_failed: Parameters indicating that the process has failed.
+
+    Each dictionary contains the following keys:
+        - "Status": A string indicating the status of the process (e.g., "InProgress", "Successful", "Failed").
+        - "uuid": The UUID of the process.
+        - "TableName": The name of the table associated with the process.
+    """
+    status_params_inprogress = {
+        "Status": ("str", "InProgress"),
+        "uuid": ("str", f'{uuid}'),
+        "TableName": ("str", f'{oc_args_json["table_name"]}')
+    }
+    status_params_success = {
+        "Status": ("str", "Successful"),
+        "uuid": ("str", f'{uuid}'),
+        "TableName": ("str", f'{oc_args_json["table_name"]}')
+    }
+    status_params_failed = {
+        "Status": ("str", "Failed"),
+        "uuid": ("str", f'{uuid}'),
+        "TableName": ("str", f'{oc_args_json["table_name"]}')
+    }
+    return status_params_inprogress, status_params_success, status_params_failed
+
+
+def extract_ssn(oc_args_json, parsed_form):
+    """
+    Extracts the Social Security Number (SSN) from the parsed form data based on the provided webform ID.
+
+    Args:
+        oc_args_json (dict): A dictionary containing various process-related arguments, including the webform ID.
+        parsed_form (dict): A dictionary containing the parsed form data, including potential SSN fields.
+
+    Returns:
+        str or None: The extracted SSN as a string with hyphens removed, or None if the SSN is not present in the form data.
+    """
+    match oc_args_json['os2form_webform_id']:
+        case "tilmelding_til_modersmaalsunderv" | "indmeldelse_i_modtagelsesklasse" | "ansoegning_om_koersel_af_skoleel" | "ansoegning_om_midlertidig_koerse":
+            if 'cpr_barnets_nummer' in parsed_form['data']:
+                return parsed_form['data']['cpr_barnets_nummer'].replace('-', '')
+            if 'barnets_cpr_nummer' in parsed_form['data']:
+                return parsed_form['data']['barnets_cpr_nummer'].replace('-', '')
+        case _:
+            return None
 
 
 if __name__ == "__main__":
