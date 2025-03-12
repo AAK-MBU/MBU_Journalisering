dd157c58ae34130e0a90dc16cd14ff74a8438fcb
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-03-12 09:42:30 +0100

    updated mbu_journalising with RKU latest changes to template robot-framework repo - still missing full implementation of servicenow api

--- robot_framework/exceptions.py
+++ robot_framework/exceptions.py
@@ -1,4 +1,5 @@
 """This module contains various functions and classes to handle errors in the framework."""
+import json
 
 import traceback
 
@@ -8,12 +9,14 @@ from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConn
 from robot_framework import config
 from robot_framework import error_screenshot
 
+# from robot_framework import service_now_handler  # Uncomment when ServiceNow API is implemented
+
 
 class BusinessError(Exception):
     """An empty exception used to identify errors caused by breaking business rules"""
 
 
-def handle_error(message: str, error: Exception, queue_element: QueueElement | None, orchestrator_connection: OrchestratorConnection) -> None:
+def handle_error(message: str, error_count: str | None, error: Exception, queue_element: QueueElement | None, orchestrator_connection: OrchestratorConnection) -> None:
     """Handles an error caught during the process.
     Logs an error to OpenOrchestrator.
     Marks the queue element (if any) as failed.
@@ -25,13 +28,40 @@ def handle_error(message: str, error: Exception, queue_element: QueueElement | N
         queue_element: The queue element to fail, if any.
         orchestrator_connection: A connection to OpenOrchestrator.
     """
-    error_msg = f"{message}: {repr(error)}\n\nTrace:\n{traceback.format_exc()}"
+    error_dict = {
+        "type": message,
+        "error_count": error_count,
+        "message": str(error),
+        "trace": traceback.format_exc()
+    }
+    error_msg = json.dumps(error_dict, ensure_ascii=False)
     error_email = orchestrator_connection.get_constant(config.ERROR_EMAIL).value
 
     orchestrator_connection.log_error(error_msg)
     if queue_element:
         orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.FAILED, error_msg)
     error_screenshot.send_error_screenshot(error_email, error, orchestrator_connection.process_name)
+
+    if message == "ApplicationException":
+        print("IMPLEMENT POST INCIDENT FUNCTIONALITY HERE")
+
+        orchestrator_connection.log_trace("ApplicationException caught. Missing implementation of ServiceNow API")
+
+        # try:
+        #     orchestrator_connection.log_trace("ApplicationException caught. Trying to create ServiceNow incident.")
+
+        #     service_now_api_username = orchestrator_connection.get_credential(config.SERVICE_NOW_API_PROD_USER).username
+        #     service_now_api_password = orchestrator_connection.get_credential(config.SERVICE_NOW_API_PROD_USER).password
+
+        #     service_now_handler.post_incident(service_now_api_username, service_now_api_password, error_dict)
+
+        #     orchestrator_connection.log_trace("ServiceNow incident created.")
+
+        # # pylint: disable-next = broad-exception-caught
+        # except Exception as e:
+        #     print(f"Failed to create ServiceNow incident: {e}")
+
+        #     orchestrator_connection.log_error(f"Failed to create ServiceNow incident. error_msg: {error_msg}")
 
 
 def log_exception(orchestrator_connection: OrchestratorConnection) -> callable:
